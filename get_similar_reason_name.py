71aa8ac984bcea0d332f7a647c5a6cfaddc071cf
marvin <mawentao03@baidu.com>
2016-09-04 21:16:36 +0800

    test

--- get_similar_reason_name.py
+++ get_similar_reason_name.py
@@ -1,12 +1,14 @@
 #coding=utf8
 import Levenshtein
+import os
 import codecs
+import sys
 
 
-def main():
+def main(input, output):
 	result = []
 	wrong_data = []
-	with codecs.open("./data/reason_name_file", encoding="utf-8") as f:
+	with codecs.open(input, encoding="utf-8") as f:
 		for row in f.readlines():
 			eid,reason,name = row.strip().split('\t')
 			lev_dis = Levenshtein.distance(name.lower(), reason.lower())
@@ -22,16 +24,18 @@ def main():
 
 			coff = float(lev_dis - len_dis)/shorter
 			if coff < 0.5:
-				result.append([eid, name, reason])
+				result.append([eid, name, reason, str(coff)])
 
-	with codecs.open("./data/result_file", 'w', encoding='utf-8') as co:
+	with codecs.open(output, 'w', encoding='utf-8') as co:
 		for row in result:
 			co.write("\t".join(row) + '\n')
 
-	with codecs.open("./data/wrong_file", 'w', encoding='utf-8') as cw:
+	with codecs.open(os.path.join(os.path.dirname(__file__),"./data/wrong_file"), 'w', encoding='utf-8') as cw:
 		for row in wrong_data:
 			cw.write("\t".join(row) + '\n')
 
 
 if __name__ == '__main__':
-	main()
+	input= sys.argv[1]
+	output = sys.argv[2]
+	main(input, output)
