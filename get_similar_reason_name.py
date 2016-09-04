4e3e607056d11708d6cceb2fb389dd12ddf2c86e
mawentao03 <mawentao03@baidu.com>
2016-09-04 20:25:28 +0800

    zero

--- get_similar_reason_name.py
+++ get_similar_reason_name.py
@@ -5,6 +5,7 @@ import codecs
 
 def main():
 	result = []
+	wrong_data = []
 	with codecs.open("./data/reason_name_file", encoding="utf-8") as f:
 		for row in f.readlines():
 			eid,reason,name = row.strip().split('\t')
@@ -13,6 +14,9 @@ def main():
 			lreason = len(reason)
 			lname = len(name)
 			shorter = min(lreason, lname)
+			if shorter == 0:
+				wrong_data.append([eid, name, reason])
+				continue
 			longer = max(lreason, lname)
 			len_dis = longer - shorter
 
@@ -24,6 +28,9 @@ def main():
 		for row in result:
 			co.write("\t".join(row) + '\n')
 
+	with codecs.open("./data/wrong_file", 'w', encoding='utf-8') as cw:
+		for row in wrong_data:
+			cw.write("\t".join(row) + '\n')
 
 
 if __name__ == '__main__':
